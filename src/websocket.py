6b8563779d7fefda2ad5f6ce8c11621700b62252
tubleronchik <m.bystramovich@gmail.com>
2023-12-06 22:14:53 +0100

    fix websocket connection

--- src/websocket.py
+++ src/websocket.py
@@ -8,16 +8,19 @@ from utils.decrypt_msg import decrypt_message
 load_dotenv()
 
 LIBP2P_WS_SERVER = os.getenv("LIBP2P_WS_SERVER")
+ADMIN_SEED = os.getenv("ADMIN_SEED")
 
 
 class WSClient:
     def __init__(self, odoo) -> None:
         self._logger = logger("websocket")
-        self.ws = self._connect2server()
         self.odoo = odoo
+        self.ws = self._connect2server()
+        self.ws.run_forever()
+        
 
     def _connect2server(self):
-        websocket.enableTrace(True)
+        # websocket.enableTrace(True)
         ws = websocket.WebSocketApp(
             LIBP2P_WS_SERVER,
             on_open=self._on_connection,
@@ -29,6 +32,7 @@ class WSClient:
 
     def _on_connection(self, ws):
         self._logger.DEBUG(f"Websocket: Connceted to {LIBP2P_WS_SERVER}")
+        self.ws.send(json.dumps({"protocols_to_listen": ["/initialization"]} ))
 
     def _on_message(self, ws, message):
         json_message = json.loads(message)
@@ -36,7 +40,7 @@ class WSClient:
         if not "email" in json_message:
             decrypted_email = json_message["email"]
             robonomics_address = json_message["address"]
-            encrypted_email = decrypt_message(decrypted_email)
+            encrypted_email = decrypt_message(decrypted_email, robonomics_address, ADMIN_SEED)
             self.odoo.create_rrs_user(encrypted_email, robonomics_address)
 
     def _on_error(self, ws, error):

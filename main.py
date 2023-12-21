0c2e1e1bfaa51dd1e232f93a2a7a5af46e3ae8c7
tubleronchik <m.bystramovich@gmail.com>
2023-12-21 23:39:06 +0100

    feat: reconnect to websocket server using dispatcher

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import threading
-
+import os
 from src.odoo import OdooProxy
 from src.websocket import WSClient
 from src.http_server import OdooFlaskView, BaseView
@@ -12,7 +12,10 @@ def main() -> None:
     ws = WSClient(odoo)
     BaseView.initialize(odoo, ws)
     OdooFlaskView.register(app, route_base="/")
-    threading.Thread(target=lambda: app.run(host="127.0.0.1", port=5000)).start()
+    flask_thread = threading.Thread(target=lambda: app.run(host="127.0.0.1", port=5000))
+    flask_thread.start()
+    ws.run()
+    os._exit(0)
 
 
 if __name__ == "__main__":
